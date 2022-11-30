08a0527666f3ba96f3d151142b5660d5eb9028fd
liningsdu <liningsdu@gmail.com>
2022-11-29 22:15:27 -0500

    Add message for print

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 def main(msg):
+    // print a message
     print("Hello {}!".format(msg))
 
 main("World")
